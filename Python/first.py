a27f3ed140b4d062cd18bae2fac45ba8aa9a5a36
Akash <akashj.vasava@gmail.com>
2026-05-25 22:24:43 +0530

    Started DSA in python

---
+++ Python/first.py
@@ -0,0 +1,7 @@
+print("Hello world");
+
+# Comment in python
+
+"""Doc String in python
+This is used to write the documentation of the code
+This is a multi line comment"""
---
+++ Python/second.py
@@ -0,0 +1,6 @@
+name = "Akash";
+print("Hello " + name)
+
+# Can't use nubmer at the start of variable name
+# Can't use space in variable name
+# Can't use special characters in variable name except _
