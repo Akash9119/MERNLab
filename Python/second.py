becf3efe738c6ac2015206d6c934b05bbb90b219
Akash <akashj.vasava@gmail.com>
2026-05-25 22:26:44 +0530

    Started DSA in python

--- Python/second.py
+++ Python/second.py
@@ -4,3 +4,8 @@ print("Hello " + name)
 # Can't use nubmer at the start of variable name
 # Can't use space in variable name
 # Can't use special characters in variable name except _
+
+
+UserName = "Akash" #Pascal Case
+userName = "Akash" #Camel Case
+user_name = "Akash" #Snake Case
