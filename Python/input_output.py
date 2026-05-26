9523f94db8918fc1398ea287b53909dca5f51e5d
Akash <akashj.vasava@gmail.com>
2026-05-26 08:46:06 +0530

    completed input and output

---
+++ Python/input_output.py
@@ -0,0 +1,14 @@
+name = "Akash"
+age = 25
+
+print("My name is " + name + " and my age is " + str(age))
+print("My name is " + name)
+print(f"My name is {name} and age is {age}")
+
+
+
+user_name = input("Enter your name: ")
+print(f"Hello {user_name}")
+
+user_age = int(input("Enter your age: "))
+print(f"Your age is {user_age}")
