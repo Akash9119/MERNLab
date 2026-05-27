b338a0a8c3fe22ebe62a35aae2513439093a0d4e
Akash <akashj.vasava@gmail.com>
2026-05-27 08:55:40 +0530

    Completed reverse number

--- Python/reverse_number.py
+++ Python/reverse_number.py
@@ -1,9 +1,10 @@
 # Accept the number and print the number in reverse
 
-n = int(print("Enter the number to see reverse: "))
+n = int(input("Enter the number to see reverse: "))
 
-new_n = 0;
+rev_n = 0
 
 while n > 0:
-    digit = n % 10
-    new_n = digit * n
+    rev_n = rev_n * 10 + n % 10
+    n = int(n/10)
+print(rev_n)
