ddf4eb49e8978753416f5b467f96d46b29568312
Akash <akashj.vasava@gmail.com>
2026-06-01 22:23:46 +0530

    Getting attribute and method from a class

--- Python/class.py
+++ Python/class.py
@@ -4,5 +4,14 @@
 class Factory:
     a = 12 #attribute
     
-    def hello(): #method
+    def hello(self): #method
         print("Hello from class")
+    
+    print("Print on the initialization of class i.e. on the first run of the program")
+
+# To create a object from a class call the class
+Factory()
+
+# To print attribute inside a class
+print(Factory.a)
+Factory().hello()
