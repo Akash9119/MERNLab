085fca2fdb9828d86a942db6530b32ccc75f70d8
Akash <akashj.vasava@gmail.com>
2026-06-02 22:32:00 +0530

    Updated encapsulation in python

--- Python/encapsulate.py
+++ Python/encapsulate.py
@@ -1,4 +1,4 @@
 # no undersore makes the attribute or method public
-# use _ to make the attribute or method protected
+# use _ to make the attribute or method protected (Just for naming convention, actully python don't have protected it works same as public)
 # Use __ to make the attribute or method private
 
