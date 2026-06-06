14acfebfcb6831a0a51c57a7849559054965a43d
Akash <akashj.vasava@gmail.com>
2026-06-06 16:34:36 +0530

    Refectored the structure

--- DSA/remove_duplicate_from_array.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        k = 0
-        for num in nums:
-            for i in range(nums.count(num)-1):
-                nums.remove(num)
-            k += 1
-
-        return k
