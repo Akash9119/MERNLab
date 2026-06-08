acbe208a1311017e148a68e58361507de11ec0f9
Akash <akashj.vasava@gmail.com>
2026-06-08 09:22:37 +0530

    Completed User registration flow

--- Auth/src/controller/user.controller.js
+++ Auth/src/controller/user.controller.js
@@ -1,7 +1,25 @@
 const user = require('../model/user.model')
 
 const registerUser = async (req, res) => {
-
+    const {user_name, email, password} = req.body
+    const newUser = {
+        user_name,
+        email,
+        password
+    }
+    try{
+        const userData = await user.create(newUser)
+        res.status(201).json({
+            message: 'User resgistered successfully',
+            data: userData
+        })
+    }
+    catch(err) {
+        console.log("Error in registering user", err)
+        res.status(500).json({
+            message: 'Error in registering user'
+        })
+    }
 }
 
 
