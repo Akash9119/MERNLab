25f1479680b01530bbc6821980d4718b0bcd5cdc
Akash <akashj.vasava@gmail.com>
2026-06-08 09:37:10 +0530

    Completed Auth with user registration and cookie storage

--- Auth/src/controller/user.controller.js
+++ Auth/src/controller/user.controller.js
@@ -1,4 +1,6 @@
 const user = require('../model/user.model')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const registerUser = async (req, res) => {
     const {user_name, email, password} = req.body
@@ -8,7 +10,24 @@ const registerUser = async (req, res) => {
         password
     }
     try{
+        const existingUser = await user.findOne({email})
+        if(existingUser) {
+            return res.status(400).json({
+                message: 'User with this email already exists'
+            })
+        }
         const userData = await user.create(newUser)
+        const token = jwt.sign({
+            id: userData._id,
+            email: userData.email
+        }, process.env.JWT_SECRET)
+
+        const cookieOptions = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+        }
+        res.cookie('token', token, cookieOptions)
         res.status(201).json({
             message: 'User resgistered successfully',
             data: userData
