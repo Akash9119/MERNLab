a12271fb8f68da24eb297035b894c3ad1369cc4b
Akash <akashj.vasava@gmail.com>
2026-06-10 23:57:43 +0530

    Created route, model and controller for user

---
+++ PayLedger/src/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : [true, "Name is required to create a account"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required to create a account"],
+        unique: [true, "Email already exists"],
+        lowercase: true,
+        trim: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please enter valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required to create a account"],
+        minlength: [6, "Password must be atleast 6 charcters"]
+    }
+}, {
+    timestamps: true
+}
+)
+
+userSchema.pre("save", async function() {
+    (!this.isModified('password'))
+        return
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    return
+})
+
+userSchema.method.comparePassword = async function () {
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = new mongoose.model('user', userSchema)
+
+module.exports = userModel
